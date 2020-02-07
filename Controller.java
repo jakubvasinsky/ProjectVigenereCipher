1f08f9081e70aadf0078a7d6085a380ccb277187
jakubvasinsky <55527876+jakubvasinsky@users.noreply.github.com>
2020-02-07 11:02:24 +0100

    added encrypt, decrypt

--- Controller.java
+++ Controller.java
@@ -5,56 +5,116 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
+import java.util.Scanner;
 
 public class Controller {
+    public File selectedFile;
     public Button btn2;
     public Button btn3;
     public Button btn1;
     public TextArea txtarea;
-
-    public void Button2Action(ActionEvent actionEvent) throws Exception {
-     /*   String strData="";
-
-        try {
-            SecretKeySpec skeyspec=new SecretKeySpec(strKey.getBytes(),"Blowfish");
-            Cipher cipher=Cipher.getInstance("Blowfish");
-            cipher.init(Cipher.DECRYPT_MODE, skeyspec);
-            byte[] decrypted=cipher.doFinal(strEncrypted.getBytes());
-            strData=new String(decrypted);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new Exception(e);
-        }
-        return strData;*/
-    }
-
-    public void Button1Action(ActionEvent actionEvent) throws Exception{
-      /*  String strData="";
-
-        try {
-            SecretKeySpec skeyspec=new SecretKeySpec(strData.getBytes(),"Blowfish");
-            Cipher cipher=Cipher.getInstance("Blowfish");
-            cipher.init(Cipher.ENCRYPT_MODE, skeyspec);
-            byte[] encrypted=cipher.doFinal(strData.getBytes());
-            strData=new String(encrypted);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new Exception(e);
-        }*/
-
-    }
+    String data = "";
+    String newData = "";
+    String key = "";
 
     public void Button3Action(ActionEvent actionEvent) {
         FileChooser fc = new FileChooser();
-        File selectedFile = fc.showOpenDialog(null);
-        if(selectedFile !=null){
-
-        }
+        selectedFile = fc.showOpenDialog(null);
 
     }
+
+
+    public void Button2Action(ActionEvent actionEvent) throws Exception {
+        Scanner fileReader = new Scanner(selectedFile);
+        char character;
+        int ascicode;
+
+
+        key = txtarea.getText().toLowerCase();
+
+        char keyAsciChar;
+        int keyAsciInt;
+        while (fileReader.hasNextLine()) {
+            data = fileReader.nextLine();
+            System.out.println("Content in the file before decryption: "+data);
+        }
+        int j=0;
+        for (int i = 0; i < data.length(); i++) {
+            character = data.charAt(i);
+            ascicode = character;
+            if (ascicode >= 65 && ascicode <= 90 || ascicode >= 97 && ascicode <= 122) {
+
+                if (key.length()==j){
+
+                    j=0;
+                }
+
+                keyAsciChar= key.charAt(j);
+                keyAsciInt=keyAsciChar;
+                keyAsciInt=keyAsciInt-97;
+
+                ascicode = keyAsciInt-ascicode;
+
+
+                if (ascicode > 90 && ascicode < 97 || ascicode > 122) {
+                    ascicode -= 26;
+                }
+                character = (char) ascicode;
+                j++;
+            }
+            newData += character;
+        }
+        System.out.println("Decrypted content from the file: "+newData);
+
+
+    }
+
+    public void Button1Action(ActionEvent actionEvent) throws Exception {
+        Scanner fileReader = new Scanner(selectedFile);
+        char character;
+        int ascicode;
+
+
+        key = txtarea.getText().toLowerCase();
+
+        char keyAsciChar;
+        int keyAsciInt;
+        while (fileReader.hasNextLine()) {
+            data = fileReader.nextLine();
+            System.out.println("Content in the file before encryption: "+data);
+        }
+        int j=0;
+        for (int i = 0; i < data.length(); i++) {
+            character = data.charAt(i);
+            ascicode = character;
+            if (ascicode >= 65 && ascicode <= 90 || ascicode >= 97 && ascicode <= 122) {
+
+                if (key.length()==j){
+
+                    j=0;
+                }
+
+                keyAsciChar= key.charAt(j);
+                keyAsciInt=keyAsciChar;
+                keyAsciInt=keyAsciInt-97;
+
+                ascicode = keyAsciInt+ascicode;
+
+
+                if (ascicode > 90 && ascicode < 97 || ascicode > 122) {
+                    ascicode -= 26;
+                }
+                character = (char) ascicode;
+                j++;
+            }
+            newData += character;
+        }
+        System.out.println("Encrypted content from the file: "+newData);
+
+    }
+
+
+
+
 }
